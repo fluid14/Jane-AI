dc255daf4e87b10e10a3df0538dec70757e4c1ff
Jakub Dolata <jdolata5@gmail.com>
2023-06-26 18:41:27 +0200

    add back button

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import * as styles from './Header.module.sass';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear } from '@fortawesome/free-solid-svg-icons';
+import { faGear, faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export const Header = () => {
+  const { route } = useRouter();
+
   return (
     <header className={styles.header}>
       <Link href='/' className={styles.avatarWrap}>
         <div className={styles.avatar} />
         <h1 className={styles.name}>Jane AI</h1>
       </Link>
-      <Link href='settings' className={styles.settings}>
-        <FontAwesomeIcon icon={faGear} />
-      </Link>
+      {route !== '/settings' ? (
+        <Link href='settings' className={styles.settings}>
+          <FontAwesomeIcon icon={faGear} />
+        </Link>
+      ) : (
+        <Link href='/' className={styles.settings}>
+          <FontAwesomeIcon icon={faArrowLeftLong} />
+        </Link>
+      )}
     </header>
   );
 };
