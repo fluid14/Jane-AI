f3d6ba3124ba94d5ac32692248b2d4adefc389ec
Jakub Dolata <jdolata5@gmail.com>
2023-07-02 09:25:47 +0200

    add devault shortcuts

--- src/hooks/useShortcuts.jsx
+++ src/hooks/useShortcuts.jsx
@@ -9,23 +9,35 @@ export const useShortcuts = () => {
   const { actions, setActiveAction } = useContext(ActionsContext);
   const { setMessageText } = useContext(MessageContext);
 
+  const registerShortcut = async (shortcut, action) => {
+    if (await isRegistered(shortcut)) {
+      console.warn('Shortcut already registered');
+    } else {
+      await register(shortcut, async () => {
+        setActiveAction(action);
+        await openAppWindow();
+      });
+    }
+  };
+
   const openAppWindow = async () => {
     await invoke('toggle_window');
     const clipboardText = await readText();
     setMessageText(clipboardText);
   };
 
+  const defaultShortcuts = async () => {
+    const shortcuts = [{ shortcut: 'CommandOrControl+Shift+C', action: null }];
+    for (const { shortcut, action } of shortcuts) {
+      await registerShortcut(shortcut, action);
+    }
+  };
+
   const initShortcuts = async () => {
+    await defaultShortcuts();
     for (const action of actions.filter(({ shortcut: filterShortcut }) => filterShortcut)) {
       const { shortcut } = action;
-      if (await isRegistered(shortcut)) {
-        console.warn('Shortcut already registered');
-      } else {
-        await register(shortcut, async () => {
-          setActiveAction(action);
-          await openAppWindow();
-        });
-      }
+      await registerShortcut(shortcut, action);
     }
   };
 
